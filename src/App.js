9a727badb7ef5a12a0cbd1107228bd52620677a5
Kagorora <kagororamaxime@gmail.com>
2021-01-11 21:50:55 +0200

    Added Product list design

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Container } from "react-bootstrap";
 import Header from "./components/reusable/Header/Header";
 import Footer from "./components/reusable/Footer/Footer";
-import { Container } from "react-bootstrap";
+import Buyer from "./components/buyer/Buyer";
 
 const App = () => {
   return (
@@ -9,7 +10,7 @@ const App = () => {
       <Header />
       <main className='py-3'>
         <Container>
-          <h1>DMS</h1>
+          <Buyer />
         </Container>
       </main>
       <Footer />
