843a9acea06adf09b4faf314cb12907950a10d0b
Kagorora <kagororamaxime@gmail.com>
2021-01-12 10:28:22 +0200

    added reviews

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from "./components/reusable/Header/Header";
 import Footer from "./components/reusable/Footer/Footer";
 import Buyer from "./components/buyer/Buyer";
+import ProductDetails from "./components/productDetails/ProductDetails";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className='py-3'>
         <Container>
-          <Buyer />
+          <Route path='/' component={Buyer} exact />
+          <Route path='/product/:id' component={ProductDetails} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
