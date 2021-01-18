e82eaf023a01b9bdbcd74d00b55cb5d47f5e5922
Kagorora <kagororamaxime@gmail.com>
2021-01-18 20:07:21 +0200

    added logout functionality

--- src/store/actions/usersActions.js
+++ src/store/actions/usersActions.js
@@ -3,6 +3,7 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_LOGOUT,
 } from '../types/types.js';
 
 export const login = (email, password) => async (dispatch, getState) => {
@@ -36,4 +37,12 @@ export const login = (email, password) => async (dispatch, getState) => {
           : error.message,
     });
   }
+};
+
+export const logout = () => async (dispatch) => {
+  dispatch({
+    type: USER_LOGOUT,
+  });
+
+  localStorage.removeItem('userInfo');
 };
